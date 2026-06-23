8b2b0d2abf67be0a75ce9fd8c1b48d07dc2f26e0
Mateus Ryu Yamaguchi <mateus.ryu8@gmail.com>
2026-06-23 18:59:35 -0300

    feat: 4-3

---
+++ exercises/4/4_3/4_3.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	slice := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	for i, v := range slice {
+		fmt.Printf("Index: %d, Value: %d\n", i, v)
+	}
+
+	fmt.Printf("%T\n\n", slice)
+
+	fmt.Println(slice[:3])
+	fmt.Println(slice[4:])
+	fmt.Println(slice[1:7])
+	fmt.Println(slice[2:9])
+	fmt.Println(slice[2 : len(slice)-1])
+}
